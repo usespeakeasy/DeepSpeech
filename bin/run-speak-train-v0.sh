481d407075dabf0c095986a3f5eac4390ca67ba5
Deniz <dzorlu@medium.com>
2021-02-17 13:20:12 -0800

    next run with finetuning. larger batch size. reduce lr on p

--- bin/run-speak-train-v0.sh
+++ bin/run-speak-train-v0.sh
@@ -14,14 +14,19 @@ python -u DeepSpeech.py --noshow_progressbar \
   --train_files /data/training_features.csv \
   --test_files /data/test_features.csv \
   --dev_files /data/valid_features.csv \
-  --train_batch_size 16 \
-  --dev_batch_size 16 \
-  --test_batch_size 16 \
-  --n_hidden 100 \
+  --train_batch_size 32 \
+  --dev_batch_size 32 \
+  --test_batch_size 32 \
+  --learning_rate 0.0001 \
+  --n_hidden 2048 \
+  --train_cudnn \
+  --checkpoint_dir $CHECKPOINT_DIR
   --epochs 10 \
   --export_tflite \
   --export_dir /run/model_export/ \
   --summary_dir /run/summaries/ \
   --checkpoint_dir /run/checkpoints/ \
+  --reduce_lr_on_plateau \
+  --plateau_epochs 1 \
   --test_output_file /run/test_output_files/test_output_file.json \
   "$@"
